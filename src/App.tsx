15245cbac4a43c126ec2e17b17491f8ab7bba1a7
LeoZhangYYDS <zhanglingxiao1991@gmail.com>
2024-08-28 13:13:30 +1000

    update expenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import Form from "./components/Form";
+import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
+  const expenses = [
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 20, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 30, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 40, category: "Utilities" },
+  ];
   return (
     <div>
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={() => console.log("delete")} />
     </div>
   );
 };
