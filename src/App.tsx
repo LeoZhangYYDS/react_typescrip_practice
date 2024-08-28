4d4ebf5dda32c166017f0d305d0b7fee47aab686
LeoZhangYYDS <zhanglingxiao1991@gmail.com>
2024-08-28 14:11:48 +1000

    add filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 20, category: "Utilities" },
     { id: 3, description: "ccc", amount: 30, category: "Utilities" },
     { id: 4, description: "ddd", amount: 40, category: "Utilities" },
   ]);
+
+  // set category为用户选择category
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  //当selectedCategory非空时，用户选择了一个类别， filter遍历整个expenses Array,生成新的Array
+  //新的Array为 category和 selectedCategory相同的，否则显示整个expenses Array
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   // 当onDelete被调用时，会传入一个id参数， 遍历expenses Array，创建一个新的不包含当前id的新Array
   //就是说会排除传入的 id，setExpenses为新的 expenses
@@ -17,7 +30,11 @@ const App = () => {
 
   return (
     <div>
-      <ExpenseList expenses={expenses} onDelete={handleDelete} />
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={handleCategorySelect} />
+      </div>
+
+      <ExpenseList expenses={visibleExpenses} onDelete={handleDelete} />
     </div>
   );
 };
